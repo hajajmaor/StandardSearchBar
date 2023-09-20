d99cbb3ed46a140f1ddf1e8677b03f403a01bde9
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-20 19:30:54 +0200

    Update Suggestions Box with InkWell and Material

--- lib/standard_suggestions_box.dart
+++ lib/standard_suggestions_box.dart
@@ -6,37 +6,63 @@ class StandardSuggestionsBox extends StatelessWidget {
     required this.suggestions,
     required this.borderRadius,
     required this.backgroundColor,
+    required this.onSuggestionSelected,
+    required this.onTapInside,
+    required this.onTapOutside,
   });
 
   final List<String> suggestions;
   final double borderRadius;
   final Color backgroundColor;
+  final Function(String) onSuggestionSelected;
+  final Function(PointerEvent) onTapInside;
+  final Function(PointerEvent) onTapOutside;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 175,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(borderRadius),
-          bottomRight: Radius.circular(borderRadius),
+    return TapRegion(
+      onTapOutside: onTapOutside,
+      onTapInside: onTapInside,
+      child: Container(
+        height: 175,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(borderRadius),
+            bottomRight: Radius.circular(borderRadius),
+          ),
         ),
-      ),
-      child: Material(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(borderRadius),
-          bottomRight: Radius.circular(borderRadius),
-        ),
-        color: Colors.transparent,
-        child: ListView.builder(
-          // physics: NeverScrollableScrollPhysics(),
-          itemCount: suggestions.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(suggestions[index]),
-            );
-          },
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 18.0),
+          child: Material(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(borderRadius),
+              bottomRight: Radius.circular(borderRadius),
+            ),
+            color: Colors.transparent,
+            child: ListView.builder(
+              itemCount: suggestions.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    onSuggestionSelected(suggestions[index]);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    child: Text(
+                      suggestions[index],
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
